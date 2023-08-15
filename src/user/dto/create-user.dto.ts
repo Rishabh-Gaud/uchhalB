77bfc329fc6e510ca778402bd51a8173dca57abb
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-08-15 10:25:12 +0530

    initialise with appModule and updated dto and enitity of user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+// user.dto.ts
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
+
+  @IsOptional()
+  @IsString()
+  last_name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsNumber()
+  pincode: number;
+
+  @IsOptional()
+  @IsString()
+  address: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+// user.entity.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: false })
+  last_name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: false })
+  address: string;
+
+  @Prop({ required: false, minlength: 6 })
+  pincode: number;
+
+  @Prop({ default: false })
+  isVerified: boolean;
+
+  @Prop()
+  verificationCode: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
