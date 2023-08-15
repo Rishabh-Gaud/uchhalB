eb12ddda19a05e73a9399646843b406bad62615b
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-08-15 10:14:07 +0530

    mongo db connection enable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongoCreds } from './config/mongo';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoCreds.SECRET_KEY)],
   controllers: [AppController],
   providers: [AppService],
 })
