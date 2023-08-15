ebfaaedef95af03431dd7f7539f1f709517c2597
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-08-15 10:16:34 +0530

    user resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongoCreds } from './config/mongo';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(mongoCreds.SECRET_KEY)],
+  imports: [MongooseModule.forRoot(mongoCreds.SECRET_KEY), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
