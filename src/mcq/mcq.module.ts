6334d8f23595a715a5a83091522e510608107c94
kapil <kapilchaudhary8280@gmail.com>
2023-09-01 14:33:46 +0530

    added get topics of a subject

--- src/mcq/mcq.module.ts
+++ src/mcq/mcq.module.ts
@@ -7,11 +7,20 @@ import { MCQ, MCQSchema } from './entities/mcq.entity';
 import { MathMCQ, MathMCQSchema } from './entities/mathmcq.entity';
 import {CPP, CPPSchema} from './entities/CodingLanguages/cpp.entity'
 import { CPrograming, CProgramingSchema } from './entities/CodingLanguages/cProgramming.entity';
+import { ArithmaticAptitude, ArithmaticAptitudeSchema } from './entities/TechAptitude/arithmaticAptitude.entity';
+import { DataInterpretation, DataInterpretationSchema } from './entities/TechAptitude/dataInterpretation.entity';
+import { LogicalReasoning, LogicalReasoningSchema } from './entities/TechAptitude/logicalReasoning.entity';
+import { NonVerbalReasoning, NonVerbalReasoningSchema } from './entities/TechAptitude/nonVerbalReasoning.entity';
+import { VerbalReasoning, VerbalReasoningSchema } from './entities/TechAptitude/VarbalReasoning.entity';
+import { VerbalAbility, VerbalAbilitySchema } from './entities/TechAptitude/verbalAbility.entity';
 
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: MCQ.name, schema: MCQSchema }, {name: MathMCQ.name, schema:MathMCQSchema},
-  {name:CPP.name, schema:CPPSchema}, {name:CPrograming.name, schema:CProgramingSchema}])],
+  {name:CPP.name, schema:CPPSchema}, {name:CPrograming.name, schema:CProgramingSchema},
+  {name:ArithmaticAptitude.name, schema:ArithmaticAptitudeSchema},{name:DataInterpretation.name, schema:DataInterpretationSchema},
+  {name:LogicalReasoning.name, schema:LogicalReasoningSchema}, {name:NonVerbalReasoning.name, schema:NonVerbalReasoningSchema},
+  {name:VerbalReasoning.name, schema:VerbalReasoningSchema}, {name:VerbalAbility.name, schema:VerbalAbilitySchema}])],
   controllers: [McqController],
   providers: [McqService],
 })
--- src/mcq/mcq.service.ts
+++ src/mcq/mcq.service.ts
@@ -4,6 +4,14 @@ import { UpdateMcqDto } from './dto/update-mcq.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { MCQ, MCQDocument } from './entities/mcq.entity';
 import {MathMCQDocument, MathMCQ} from './entities/mathmcq.entity';
+import {CPP, CPPDocument} from './entities/CodingLanguages/cpp.entity'
+import { CPrograming, CProgramingDocument } from './entities/CodingLanguages/cProgramming.entity';
+import { ArithmaticAptitude, ArithmaticAptitudeDocument } from './entities/TechAptitude/arithmaticAptitude.entity';
+import { DataInterpretation, DataInterpretationDocument } from './entities/TechAptitude/dataInterpretation.entity';
+import { LogicalReasoning, LogicalReasoningDocument } from './entities/TechAptitude/logicalReasoning.entity';
+import { NonVerbalReasoning, NonVerbalReasoningDocument } from './entities/TechAptitude/nonVerbalReasoning.entity';
+import { VerbalReasoning, VerbalReasoningDocument } from './entities/TechAptitude/VarbalReasoning.entity';
+import { VerbalAbility, VerbalAbilityDocument } from './entities/TechAptitude/verbalAbility.entity';
 import { Model } from 'mongoose';
 
 
@@ -13,11 +21,46 @@ export class McqService {
     @InjectModel(MCQ.name)
     private mcqModel: Model<MCQDocument>,
 
+    @InjectModel(CPP.name)
+    private CPPModel: Model<CPPDocument>,
+
     @InjectModel(MathMCQ.name)
-    private mathMcqModel: Model<MathMCQDocument>
+    private mathMcqModel: Model<MathMCQDocument>,
+
+    @InjectModel(CPrograming.name)
+    private CProgramingModel: Model<CProgramingDocument>,
+
+    @InjectModel(ArithmaticAptitude.name)
+    private ArithmaticAptitudeModel: Model<ArithmaticAptitudeDocument>,
+
+    @InjectModel(DataInterpretation.name)
+    private DataInterpretationModel: Model<DataInterpretationDocument>,
+
+    @InjectModel(LogicalReasoning.name)
+    private LogicalReasoningModel: Model<LogicalReasoningDocument>,
+
+    @InjectModel(NonVerbalReasoning.name)
+    private NonVerbalReasoningModel: Model<NonVerbalReasoningDocument>,
+
+    @InjectModel(VerbalReasoning.name)
+    private VerbalReasoningModel: Model<VerbalReasoningDocument>,
+
+    @InjectModel(VerbalAbility.name)
+    private VerbalAbilityModel: Model<VerbalAbilityDocument>
   ) { }
   async create(createMcqDto: CreateMcqDto) {
     try {
+      var mapping = {
+        sriram: this.mcqModel,
+        cpp:this.CPPModel,
+        cprogramming:this.CProgramingModel,
+        arithmaticaptitude:this.ArithmaticAptitudeModel,
+        datainterpretation:this.DataInterpretationModel,
+        logicalreasoning:this.LogicalReasoningModel,
+        nonverbalreasoning:this.NonVerbalReasoningModel,
+        verbalreasoning:this.VerbalReasoningModel,
+        verbalability:this.VerbalAbilityModel
+      };
       const questionCreated = await new this.mcqModel(createMcqDto);
       await questionCreated.save();
       return questionCreated;
@@ -41,6 +84,28 @@ export class McqService {
   async findAll() {
     try {
       const data = await this.mcqModel.find().exec();
+      return data;
+    } catch (error) {
+      console.log('[SERVER ERROR][McqService:findAll]: ', error);
+      throw error;
+    }
+  }
+
+  async gettopics(subject:string) {
+    try {
+      var mapping = {
+        sriram: this.mcqModel,
+        cpp:this.CPPModel,
+        cprogramming:this.CProgramingModel,
+        arithmaticaptitude:this.ArithmaticAptitudeModel,
+        datainterpretation:this.DataInterpretationModel,
+        logicalreasoning:this.LogicalReasoningModel,
+        nonverbalreasoning:this.NonVerbalReasoningModel,
+        verbalreasoning:this.VerbalReasoningModel,
+        verbalability:this.VerbalAbilityModel
+      };
+      console.log(subject);
+      const data = await mapping[subject].distinct("category").exec();
       return data;
     } catch (error) {
       console.log('[SERVER ERROR][McqService:findAll]: ', error);
